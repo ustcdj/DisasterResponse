8014945c6fc6225f9a8a7ed08aceee0900e71338
Dong Jiang <ustcdj@gmail.com>
2020-04-14 11:12:08 -0500

    put main() back, app running fine

--- app/run.py
+++ app/run.py
@@ -119,11 +119,10 @@ def go():
         classification_result=classification_results
     )
 
-'''
+
 def main():
     app.run(host='0.0.0.0', port=3001, debug=True)
 
 
 if __name__ == '__main__':
     main()
-'''
