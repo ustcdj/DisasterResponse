09982a77e420e45543d22ca7ab39cdbff9da32cb
Dong Jiang <ustcdj@gmail.com>
2020-04-14 00:13:42 -0500

    comment out main()

--- app/run.py
+++ app/run.py
@@ -119,10 +119,11 @@ def go():
         classification_result=classification_results
     )
 
-
+'''
 def main():
     app.run(host='0.0.0.0', port=3001, debug=True)
 
 
 if __name__ == '__main__':
     main()
+'''
