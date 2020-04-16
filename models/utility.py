39c43f1ec588cbb004dd8d93a4d1eb04cb1ca15e
Dong Jiang <ustcdj@gmail.com>
2020-04-16 10:34:23 -0500

    add download 4 nltk libraries

--- models/utility.py
+++ models/utility.py
@@ -1,6 +1,11 @@
 import re
 import pandas as pd
 import nltk
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
